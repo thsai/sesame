e4dc06437c3a0793aaaa2e8eacca2be9ed79673e
wangqi <wangqi@yiniu.com>
2017-04-27 17:25:42 +0800

    get改为post请求

--- app/src/main/java/com/qr/sesame/api/QRService.java
+++ app/src/main/java/com/qr/sesame/api/QRService.java
@@ -2,7 +2,8 @@ package com.qr.sesame.api;
 
 import com.qr.sesame.entiy.SuccessData;
 
-import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -11,13 +12,16 @@ import rx.Observable;
  */
 
 public interface QRService {
-    @GET("QRCodeServlet")
-    Observable<SuccessData> scan(@Query("method") String method,@Query("isPass") String isPass);
+    @Headers("Content-Type: application/x-www-form-urlencoded;charset=utf-8")
+    @POST("QRCodeServlet")
+    Observable<SuccessData> scan(@Query("method") String method, @Query("isPass") String isPass);
 
-    @GET("RegistServlet")
-    Observable<SuccessData> register(@Query("method") String method,@Query("idcard") String idcard,@Query("name") String name,@Query("password") String password);
+    @Headers("Content-Type: application/x-www-form-urlencoded;charset=utf-8")
+    @POST("RegistServlet")
+    Observable<SuccessData> register(@Query("method") String method, @Query("idcard") String idcard, @Query("name") String name, @Query("password") String password);
 
-    @GET("LoginServlet")
-    Observable<SuccessData> login(@Query("idcard") String idcard,@Query("name") String name,@Query("password") String password);
+    @Headers("Content-Type: application/x-www-form-urlencoded;charset=utf-8")
+    @POST("LoginServlet")
+    Observable<SuccessData> login(@Query("idcard") String idcard, @Query("name") String name, @Query("password") String password);
 
 }
